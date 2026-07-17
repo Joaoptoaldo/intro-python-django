bac9323e521ffe53100493991c5745fe3b1aee0a
João Pedro Toaldo <toaldojoaopedro@gmail.com>
2026-07-17 18:12:23 -0300

    config modelos e migrações do django

--- setup/settings.py
+++ setup/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config, Csv
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ch5il4&ym6t8z@n@d$$s%p5q&4q_&(8d-n00r6sn@%g3u&5a59'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
@@ -74,10 +76,10 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config(
+        "DATABASE_URL", default=f'sqlite:///{BASE_DIR / "db.sqlite3"}', 
+        cast=dburl,
+    )
 }
 
 
