b7ed88ab6e1a480572775c5edc3bddbddf23c1a8
João Pedro Toaldo <toaldojoaopedro@gmail.com>
2026-07-18 12:09:46 -0300

    add configuração personalizada ao django admin

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Todo
+
+
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "deadline",
+        "created_at",
+        "finished_at",
+    )
+
+    search_fields = (
+        "title",
+    )
+
+    list_filter = (
+        "deadline",
+        "finished_at",
+    )
+
+    ordering = (
+        "-created_at",
+    )
