d30207a1f2f87dbde95cc3ab9a6458fc97187b46
João Pedro Toaldo <toaldojoaopedro@gmail.com>
2026-07-18 21:50:44 -0300

    validação do deadline usando timezone.localdate()

--- tasks/models.py
+++ tasks/models.py
@@ -1,13 +1,38 @@
 from django.db import models
-from datetime import date
+from django.utils import timezone
 
-# Create your models here.
+
 class Todo(models.Model):
-    title = models.CharField(verbose_name="Titulo", max_length=100, null=False, blank=False)
-    description = models.TextField(verbose_name="Descrição", max_length=500, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(verbose_name="Data de entrega", null=False, blank=False)
-    finished_at = models.DateField(null=True, blank=True)
+    title = models.CharField(
+        verbose_name="Titulo",
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+
+    description = models.TextField(
+        verbose_name="Descrição",
+        null=False,
+        blank=True,
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        null=False,
+        blank=False,
+    )
+
+    deadline = models.DateField(
+        verbose_name="Data de entrega",
+        null=False,
+        blank=False,
+    )
+
+    finished_at = models.DateField(
+        null=True,
+        blank=True,
+    )
+
     class Meta:
         ordering = ["deadline"]
 
@@ -15,12 +40,12 @@ class Todo(models.Model):
         from django.core.exceptions import ValidationError
 
         # não salva uma tarefa com data de entrega anterior à data atual
-        if self.deadline and self.deadline < date.today():
+        if self.deadline and self.deadline < timezone.localdate():
             raise ValidationError({"deadline": "A data de entrega não pode ser anterior à data atual."})
 
         super().clean()
 
     def mark_has_complete(self):
         if not self.finished_at:
-            self.finished_at = date.today()
+            self.finished_at = timezone.localdate()
             self.save()
